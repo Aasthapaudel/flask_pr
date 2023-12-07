fe2af1c32eef9f39d9a30886d643c19010252a98
Aastha <pdlpangali111@gmail.com>
2023-12-07 23:43:02 +0545

    route setup

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 app =Flask(__name__)
 
 @app.route("/")
 def index():
-    return("Home page")
+    return render_template("index.html")
+@app.route("/main")
+def main():
+    return render_template("main.html")
 @app.route("/register")
 def register():
-    return("Register page")
+    return render_template("register.html")
 @app.route("/login")
 def login():
-    return("login page")
+    return render_template("login.html")
 
 if __name__=="__main__":
     app.run(debug=True)
