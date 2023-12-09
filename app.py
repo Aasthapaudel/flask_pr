b055ad5c25385bb32927ae87c443b8352e45703d
Aastha <pdlpangali111@gmail.com>
2023-12-09 06:48:16 +0545

    datbase

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
 app =Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb.db'
+db = SQLAlchemy(app)
+db.init_app(app)
+
 
 @app.route("/")
 def index():
