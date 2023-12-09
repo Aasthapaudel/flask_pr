d47b7fac00d8ac5fdd6f01bb05a6a2271b2a7217
Aastha <pdlpangali111@gmail.com>
2023-12-09 09:57:38 +0545

    queryset

--- app.py
+++ app.py
@@ -3,10 +3,21 @@ from flask_sqlalchemy import SQLAlchemy
 
 app =Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db = SQLAlchemy(app)
+app.app_context().push()
 
-
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    fname = db.Column(db.String, nullable=False)
+    lname = db.Column(db.String, nullable=False)
+    email = db.Column(db.String)
+    password = db.Column(db.String, nullable=False)
+    
+    
+def __repr__(self):
+        return '<User %r>' % self.username
 @app.route("/")
 def index():
     return render_template("index.html")
