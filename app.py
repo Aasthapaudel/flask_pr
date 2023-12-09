7189457517a869ce87e2b0ece3ef1ac291223bb9
Aastha <pdlpangali111@gmail.com>
2023-12-09 08:46:08 +0545

    upadte database

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
+
 app =Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db = SQLAlchemy(app)
-db.init_app(app)
 
 
 @app.route("/")
